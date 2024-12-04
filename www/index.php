98749858b000be1c0c3e7a0f6ae2b71921f4abc3
beadsalmon <beadsalmon@gmail.com>
2024-12-04 23:04:45 +0900

    commit TEST 2

--- www/index.php
+++ www/index.php
@@ -21,6 +21,7 @@ include_once(G5_PATH.'/head.php');
 
 <div class="latest_top_wr">
     <?php
+    // commit test
     // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
     // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
     // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
